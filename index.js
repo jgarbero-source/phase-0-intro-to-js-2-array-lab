885f4b658025a70ef24dc52de0e3774903b77ee3
John Garbero <john.garbero.itb@gmail.com>
2022-05-17 09:45:12 -0500

    commit tree 8ed7ba7d9fd3421dd4294e86db4c96dfebd2494e

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(x) {
+    return cats.push(x);
+}
+
+function destructivelyPrependCat(x) {
+    return cats.unshift(x)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.splice(2,1)
+}
+
+function destructivelyRemoveFirstCat(x) {
+    cats.shift(x);
+}
+
+function appendCat(x) {
+    const newCat = [...cats, x];
+    return newCat;
+}
+
+function prependCat(x) {
+    const newCat3 = [x, ...cats];
+    return newCat3
+}
+function removeLastCat() {
+    const newCat2 = cats.slice(0,2)
+    return newCat2;
+}
+
+function removeFirstCat() {
+    const newcat4 = cats.slice(1,3);
+    return newcat4;
+}
